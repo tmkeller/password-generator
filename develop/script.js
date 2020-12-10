393efc81c058b04e976c54cb7a2b2df3a1a55697
Tim Keller <timothy.m.keller@gmail.com>
2020-12-09 20:36:15 -0800

    Added console logs to investigate possible dropped characters.

--- develop/script.js
+++ develop/script.js
@@ -45,6 +45,7 @@ function generatePassword() {
       for ( var i = 0; i < charNumber; i++ ) {
         var charRand = charList[ Math.floor( Math.random() * (charList.length) ) ];
         passText += charRand;
+        console.log(passText.length);
         console.log(passText);
       }
 
